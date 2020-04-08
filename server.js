bc362c22358a47fdad2500346a9fcf0125b7f053
mushuyun <mushuyun@yahoo.com>
2020-04-07 20:10:10 -0400

    template setup

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 const express = require("express");
 const sequelize = require("sequelize");
 const app = express();
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/movie-api-routes.js")(app);
+require("./routes/dinner-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
