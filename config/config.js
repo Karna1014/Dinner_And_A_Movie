3bfdfa1613cd5b41b4d7b8dd64e96db0b84f93a1
Karna <57597042+Karna1014@users.noreply.github.com>
2020-04-13 08:14:54 -0400

    Recommit on branch

--- config/config.js
+++ config/config.js
@@ -1,13 +1,8 @@
 module.exports = {
     development: {
       username: "root",
-<<<<<<< HEAD
       password: process.env.SEQUELIZE_PASSWORD,
       database: "movie_dinner",
-=======
-      password: "Tha,bto7",
-      database: 'movie_dinner',
->>>>>>> d61e0e22ca683e70592fd8d3f87c25d734a79684
       details: {
         host: "localhost",
         port: 3306,
@@ -16,13 +11,8 @@ module.exports = {
     },
     test: {
       username: "root",
-<<<<<<< HEAD
       password: process.env.SEQUELIZE_PASSWORD,
       database: "movie_dinner_test",
-=======
-      password: "Tha,bto7",
-      database: 'movie_dinner',
->>>>>>> d61e0e22ca683e70592fd8d3f87c25d734a79684
       details: {
         host: "localhost",
         port: 3306,
