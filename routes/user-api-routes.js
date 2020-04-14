5ed01ee7f01256bef0a4ab3e642f4efc9cf037bd
Jasmin Rowdy <jrowdy@wcpss.net>
2020-04-14 10:26:07 -0400

    API connect for recipe builder

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -205,7 +205,7 @@ module.exports = function (app) {
       console.log(result);
       var genreId = result.genreId;
       var today = moment().format("YYYY-MM-DD");
-       var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=3d866c05691ba06f9fa697f8e8c9e838&language=en-US&region=US&sort_by=release_date.asc&include_video=false&page=1&primary_release_date.gte=" + today + "&with_genres=" + genreId;
+       var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=&language=en-US&region=US&sort_by=release_date.asc&include_video=false&page=1&primary_release_date.gte=" + today + "&with_genres=" + genreId;
 
        request(queryURL, function (error, response, body) {
          console.log("error:", error); // Print the error if one occurred
