ff2f76bc1ef17857461a60eec6bd975d8bcff266
Karna <57597042+Karna1014@users.noreply.github.com>
2020-04-14 21:21:51 -0400

    added config var to deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const sequelize = require("sequelize");
 const app = express();
 const fbApp = require("./config/fb-config");
 const path = require("path");
+const config = require(__dirname, + "./config/config.js")[env]
 
 
 
@@ -25,17 +26,16 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Static directory
-// app.use(express.static("public"));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 // =============================================================
-//var routes = require("./routes/html-routes.js");
+
 
 require("./routes/user-api-routes")(app);
 
-// require("./routes/movie-api-routes.js")(app);
-// require("./routes/dinner-api-routes.js")(app);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
