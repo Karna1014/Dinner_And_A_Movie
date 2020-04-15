e23c43e6a1b004abbd05d12adc7ba612b4d386c0
Karna <57597042+Karna1014@users.noreply.github.com>
2020-04-15 00:18:32 -0400

    Connection changes for Heroku

--- server.js
+++ server.js
@@ -8,10 +8,31 @@ const path = require("path");
 
 
 
-
 // setting view port
 
 var PORT = process.env.PORT || 8080;
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL) 
+  } else {
+  var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.SEQUELIZE_PASSWORD,
+  database: 'movie_dinner'
+  });
+};
+//make connection or pass error
+connection.connect(function(err) {
+  if (err) {
+      console.error("Error Connecting: " + err.stack);
+      return;
+  }
+});
+
+//Export connection for other file use
+
 
 var db = require("./models");
 
@@ -45,4 +66,3 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
