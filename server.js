cfe30791b508858379b28a5c44017bf22fba87bc
Karna <57597042+Karna1014@users.noreply.github.com>
2020-04-15 10:00:29 -0400

    heroku fixes

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 require("dotenv").config();
 
 const express = require("express");
-const sequelize = require("sequelize");
 const app = express();
-const fbApp = require("./config/fb-config");
 const path = require("path");
-const config = require("./config/config")
 const db = require("./models");
 const PORT = process.env.PORT || 3456;
-
-
-// setting view port
-
-
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,15 +17,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Static directory
-
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 // =============================================================
-
-
 require("./routes/user-api-routes")(app);
-
 
 
 // Syncing our sequelize models and then starting our Express app
