902c8ec9231bb2ed71b1992156ea66b790d035c9
Karna <57597042+Karna1014@users.noreply.github.com>
2020-04-19 16:41:08 -0400

    undid force true for tables

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ require("./routes/user-api-routes")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 // { force: false, alter: true }
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
